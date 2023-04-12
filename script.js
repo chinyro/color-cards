92877586c308319861240ee26a02c535774caf1c
sydrob <55166840+sydrobhez@users.noreply.github.com>
2023-04-11 18:31:14 -0600

    adding styles

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+let numSquares = 6;
+let colors = [];
+let pickedColor;
+
+const squares = document.querySelectorAll(".square");
+const colorDisplay = document.getElementById("colorDisplay");
+const messageDisplay = document.querySelector("#message");
+const h1 = document.querySelector("h1");
+const resetButton = document.querySelector("#reset");
+const modeButtons = document.querySelectorAll(".mode");
+
 
